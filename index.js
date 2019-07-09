ec4012d0bd90944a250c24e0e1994b520579c9d9
Griselda Balmaceda <gzbalmaceda@my365.bellevue.edu>
2019-07-09 13:51:39 -0500

    product information now renders inside the template

--- index.js
+++ index.js
@@ -31,6 +31,23 @@ console.log('will read file')
 
 ///////////////////////////////////
 ///Creating a Server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+       
+    }
+ return output
+
+}
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
@@ -40,19 +57,15 @@ const server = http.createServer((req, res) => {
     console.log(req.url)
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        })
-        res.end(tempOverview)
+        res.writeHead(200, {'Content-type': 'text/html'})
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(' ')
+        const output=tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+        res.end(output)
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT')
     } else if (pathName === '/api') {
-        res.writeHead(200, {
-            'Content-type': 'application/json'
-        })
+        res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data)
-
-
         console.log('API')
     } else {
         res.writeHead(404, {
