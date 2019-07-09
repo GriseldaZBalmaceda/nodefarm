97df063339e54fad06d5b85467a95279472c2c5c
Griselda Balmaceda <gzbalmaceda@my365.bellevue.edu>
2019-07-09 11:34:14 -0500

    creating different paths and adding headers

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+const http = require('http'); //server capabilities 
+const url = require('url');
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 
+////File Practice
+/*
 const text = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
 fs.writeFileSync('./txt/output.txt', text)
 
 
 //non-blocking
-
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error!')
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2)
         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
@@ -22,3 +27,37 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 })
 
 console.log('will read file')
+*/
+
+///////////////////////////////////
+///Creating a Server
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
+  });  
+  const dataObj=JSON.parse(data);
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW')
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT')
+    } else if (pathName === '/api') {
+            res.writeHead(200,{
+                'Content-type':'application/json'
+            })
+            res.end(data)
+  
+
+        console.log('API')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hellooo'
+        });
+        res.end('<h1>This page could not be found</h1>')
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening on port 8000')
+})
